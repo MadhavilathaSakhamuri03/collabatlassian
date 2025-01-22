f229cd4b93932473b8dc99b601093486dc57255b
Your Name <you@example.com>
2025-01-22 16:05:49 +0530

    bit bucjet model changes

--- backend/src/models/bitBucket.js
+++ backend/src/models/bitBucket.js
@@ -3,15 +3,7 @@ const mongoose = require('mongoose');
 const bitBucketPRsSchema = new mongoose.Schema({ 
     
     id: String,
-    issueId: String,
-    title: String, 
-    description: String,
-    status: String,
-    type: String,
-    projectName: String
-
-
-
+   
  });
 
  module.exports = mongoose.model( 'BitBucketPRs', bitBucketPRsSchema);
